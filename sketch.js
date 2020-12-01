c7947029c9aaa2146a0fd264f84992d4f75d5f4e
Bai03011122 <71304715+Bai03011122@users.noreply.github.com>
2020-12-01 11:50:53 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://bai03011122.github.io/109924206/model.json';
 
 
 function preload() {
